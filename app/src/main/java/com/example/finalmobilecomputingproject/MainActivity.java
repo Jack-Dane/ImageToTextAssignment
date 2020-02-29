f115a8c9768810d0bb8651a478eee68747802d16
Dane, Jack (j.dane603 <jd603@canterbury.ac.uk>
2020-02-29 20:58:43 +0000

    Added the ability to show image taken on button, but it is only a thumbnail so is very pixelated. Read google's documentation on how to save full size image.

--- app/src/main/java/com/example/finalmobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/finalmobilecomputingproject/MainActivity.java
@@ -7,27 +7,34 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageButton uiTakePictureImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uiTakePictureImageButton = findViewById(R.id.ImageScreenTextView);
     }
 
     public void openCamera(View view){
         if(askForCameraPermission()){
-            Log.d("pass2", "pass");
-            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            startActivity(intent);
-        }else{
-            Log.d("pass", "pass");
-            //permission not granted
-        }
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }//else the user hasn't granted permission
     }
 
     private boolean askForCameraPermission(){
@@ -35,10 +42,21 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.CAMERA},
                     100);
-
             return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED);
         }else{
             return true;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //get the image which was taken back from the user
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            uiTakePictureImageButton.setImageBitmap(imageBitmap);
         }
     }
 }
