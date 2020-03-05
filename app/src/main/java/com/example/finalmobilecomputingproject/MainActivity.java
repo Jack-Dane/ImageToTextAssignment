564404184c005b0ebda1a59a871800f468457bda
Dane, Jack (j.dane603 <jd603@canterbury.ac.uk>
2020-03-05 22:38:47 +0000

    Adding Image to file and placing onto image button

--- app/src/main/java/com/example/finalmobilecomputingproject/MainActivity.java
+++ app/src/main/java/com/example/finalmobilecomputingproject/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.finalmobilecomputingproject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private ImageButton uiTakePictureImageButton;
+    private String currentPhotoPath;
+    private File sourceFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +47,21 @@ public class MainActivity extends AppCompatActivity {
         if(askForCameraPermission()){
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                sourceFile = null;
+                try{
+                    sourceFile = createImageFile();
+                }catch (IOException ex){
+                    //error
+                }
+                if(sourceFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.example.android.fileprovider",
+                            sourceFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
-        }//else the user hasn't granted permission
+        }
     }
 
     private boolean askForCameraPermission(){
@@ -48,17 +75,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         //get the image which was taken back from the user
-        if(requestCode == REQUEST_IMAGE_CAPTURE){
-            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                uiTakePictureImageButton.setImageBitmap(imageBitmap);
-            }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            uiTakePictureImageButton.setImageURI(Uri.parse(currentPhotoPath));
         }
+    }
+
+    private File createImageFile() throws IOException {//from android development page: https://developer.android.com/training/camera/photobasics
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.UK).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+        //delete all images stored
+
     }
 }
