bef73a066dcb736ac18efac7d410ca4685ee7f94
Dane, Jack (j.dane603 <jd603@canterbury.ac.uk>
2020-04-11 20:19:27 +0100

    Permission, Gallery saves, Gallery chooser

--- app/src/main/java/com/example/finalmobilecomputingproject/HomeFragment.java
+++ app/src/main/java/com/example/finalmobilecomputingproject/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.finalmobilecomputingproject;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -26,25 +29,37 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.util.IOUtils;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 
 import static android.app.Activity.RESULT_OK;
+import static com.google.android.gms.common.util.IOUtils.copyStream;
 
 public class HomeFragment extends Fragment implements Observer{
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_IMAGE_EDIT = 2;
+    private static final int PICK_IMAGE = 3;
+    private static final int OPEN_CAMERA_PERMISSION = 10;
+    private static final int OPEN_EDIT_CHOOSER_PERMISSION = 11;
     private static final String ORIGIN_LANGUAGE_STATE = "originLanguage";
     private static final String TRANSLATED_LANGUAGE_STATE = "translatedLanguage";
     private static final String TAKEN_PICTURE_STATE = "takenPicture";
     private static final String ORIGIN_TEXT_STATE = "originText";
     private static final String TRANSLATED_TEXT_STATE = "translatedText";
+    private static final String PHOTO_FILE_PATH = "photoPath";
 
     //UI elements
     private ImageButton uiTakePictureImageButton;
@@ -67,8 +82,6 @@ public class HomeFragment extends Fragment implements Observer{
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        createImageFile();
-
         dbConnection = new DataBaseConnection(rootView.getContext());
 
         uiTakePictureImageButton = rootView.findViewById(R.id.ImageScreenTextView);
@@ -88,7 +101,7 @@ public class HomeFragment extends Fragment implements Observer{
         uiTakePictureImageButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                editImage();
+                longClickImage();
                 return true;
             }
         });
@@ -161,9 +174,18 @@ public class HomeFragment extends Fragment implements Observer{
             currentlyTranslatedText = savedInstanceState.getString(TRANSLATED_TEXT_STATE);
             uiMessageTextView.setText(currentlyTranslatedText);
 
+            currentPhotoPath = savedInstanceState.getString(PHOTO_FILE_PATH);
+            currentPhotoFile = new File(currentPhotoPath);
+
             if(savedInstanceState.getBoolean(TAKEN_PICTURE_STATE)){
                 uiTakePictureImageButton.setImageDrawable(null);
                 uiTakePictureImageButton.setImageURI(Uri.parse(currentPhotoPath));
+            }
+        }else{
+            try {
+                createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
@@ -178,6 +200,7 @@ public class HomeFragment extends Fragment implements Observer{
         int originIndex = uiOriginLanguageSpinner.getSelectedItemPosition();
         boolean savedImage = uiTakePictureImageButton.getDrawable() != null;
 
+        savedInstanceState.putString(PHOTO_FILE_PATH, currentPhotoPath);
         savedInstanceState.putInt(ORIGIN_LANGUAGE_STATE, originIndex);
         savedInstanceState.putInt(TRANSLATED_LANGUAGE_STATE, translatedIndex);
         savedInstanceState.putBoolean(TAKEN_PICTURE_STATE, savedImage);
@@ -206,17 +229,54 @@ public class HomeFragment extends Fragment implements Observer{
         super.onResume();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case OPEN_CAMERA_PERMISSION:
+                if(grantResults.length>0){
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+                        openCameraApp();
+                    }
+                }
+                break;
+            case OPEN_EDIT_CHOOSER_PERMISSION:
+                if(grantResults.length>0){
+                    if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+                        openEditChooser();
+                    }
+                }
+                break;
+        }
+    }
+
     private void openCamera(View view){
-        if(askForCameraPermission()){
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(Objects.requireNonNull(getActivity()).getPackageManager()) != null) {
-                Uri photoURI = FileProvider.getUriForFile(getActivity(),
-                        "com.example.finalmobilecomputingproject.provider",
-                        currentPhotoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    OPEN_CAMERA_PERMISSION);
         }else{
+            openCameraApp();
+        }
+    }
+
+    private void openCameraApp() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(Objects.requireNonNull(getActivity()).getPackageManager()) != null) {
+            try {
+                createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                    "com.example.finalmobilecomputingproject.provider",
+                    currentPhotoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else {
             Toast.makeText(this.getContext(), "Please enable the camera to take pictures", Toast.LENGTH_SHORT).show();
         }
     }
@@ -238,28 +298,55 @@ public class HomeFragment extends Fragment implements Observer{
         }
     }
 
-    private void editImage(){
-        if(currentPhotoPath != null){
-            Intent editIntent = new Intent(Intent.ACTION_EDIT);
-            editIntent.setDataAndType(Uri.parse(currentPhotoPath), "image/*");
-            editIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivityForResult(editIntent, REQUEST_IMAGE_EDIT);
+    private void longClickImage(){
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    OPEN_EDIT_CHOOSER_PERMISSION);
         }else{
-            Toast.makeText(this.getContext(), "Please take a picture first by click the square", Toast.LENGTH_SHORT).show();
+            openEditChooser();
         }
     }
 
-    private boolean askForCameraPermission(){
-        //ask for permission to camera
-        if(ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(getActivity(),
-                    new String[]{Manifest.permission.CAMERA},
-                    100);
-            return (ContextCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED);
+    private void openEditChooser(){
+        if(uiTakePictureImageButton.getDrawable() != null){
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext(), AlertDialog.THEME_DEVICE_DEFAULT_DARK);
+            dialogBuilder.setTitle("Do you want to edit or choose another image?");
+
+            dialogBuilder.setItems(new CharSequence[]
+                            {"Edit", "Select", "Cancel"},
+                    (dialog, which) -> {
+                        switch (which) {
+                            case 0:
+                                editImage();
+                                break;
+                            case 1:
+                                chooseImage();
+                                break;
+                        }
+                    });
+
+            AlertDialog dialog = dialogBuilder.create();
+            dialog.show();
         }else{
-            //if already given permission, return true
-            return true;
+            chooseImage();
         }
+    }
+
+    private void editImage(){
+        Intent editIntent = new Intent(Intent.ACTION_EDIT);
+        editIntent.setDataAndType(Uri.parse(currentPhotoPath), "image/*");
+        editIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(editIntent, REQUEST_IMAGE_EDIT);
+    }
+
+    private void chooseImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setDataAndType(Uri.parse(currentPhotoPath), "image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 
     private void save() {
@@ -285,9 +372,29 @@ public class HomeFragment extends Fragment implements Observer{
         super.onActivityResult(requestCode, resultCode, data);
 
         //get the image which was taken back from the user
-        if (resultCode == RESULT_OK && (requestCode == REQUEST_IMAGE_EDIT || requestCode == REQUEST_IMAGE_CAPTURE)) {
+        if (resultCode == RESULT_OK){
+            if(requestCode == PICK_IMAGE){
+                try{
+                    InputStream inputStream = getActivity().getContentResolver().openInputStream(data.getData());
+                    FileOutputStream fileOutputStream = new FileOutputStream(currentPhotoFile);
+
+                    FileOutputStream outputStream = new FileOutputStream(currentPhotoFile);
+                    int read;
+                    byte[] bytes = new byte[1024];
+                    while ((read = inputStream.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, read);
+                    }
+                    fileOutputStream.close();
+                    inputStream.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             uiTakePictureImageButton.setImageDrawable(null);//refresh
             uiTakePictureImageButton.setImageURI(Uri.parse(currentPhotoPath));
+            galleryAddPic();
             try {
                 readImage();
             } catch (IOException e) {
@@ -296,13 +403,16 @@ public class HomeFragment extends Fragment implements Observer{
         }
     }
 
-    private void createImageFile(){
-        // Create an image file name
-        String imageFileName = "TEMP_IMG";
-        File storageDir = Objects.requireNonNull(getActivity()).getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+    private void createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Translator_App_Image" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Translator");
+
+        if(!storageDir.exists()){
+            new File(storageDir.getPath()).mkdir();
+        }
         File image = new File(storageDir, imageFileName + ".jpg");
 
-        // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
         currentPhotoFile = image;
     }
@@ -330,5 +440,13 @@ public class HomeFragment extends Fragment implements Observer{
     public void updateTranslatedText(String text) {
         currentlyTranslatedText = text;
         uiMessageTextView.setText(text);
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getContext().sendBroadcast(mediaScanIntent);
     }
 }
